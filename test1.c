b91f3e7814b30992e735a77ca956f5a918fe7d76
erbort <cpcdly@sina.cn>
2014-01-24 13:39:38 +0800

    Update test1.c

--- test1.c
+++ test1.c
@@ -67,3 +67,4 @@ int main(int argc, char *argv[])
   printf("%d-%d-%d-%d-%d-%d\n",local->tm_year+1900, local->tm_mon+1, local->tm_mday, local->tm_hour+8, local->tm_min, local->tm_sec);
   //note :tm_hour+8,  peking time
 }
+
